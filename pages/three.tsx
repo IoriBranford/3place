514e08f17360f5c478c2aff60649904cc6b4b8bb
IoriBranford <ioribranford@gmail.com>
2023-03-20 21:13:48 -0700

    Create room.

--- pages/three.tsx
+++ pages/three.tsx
@@ -1,5 +1,6 @@
 import React, { useRef, useState } from 'react'
 import { Canvas, useFrame, ThreeElements } from '@react-three/fiber'
+import { FirstPersonControls } from '@react-three/drei'
 
 function Box(props: ThreeElements['mesh']) {
     const mesh = useRef<THREE.Mesh>(null!)
@@ -20,6 +21,35 @@ function Box(props: ThreeElements['mesh']) {
     )
 }
 
+function Room() {
+    return <object3D>
+        <mesh name='floor' position={[0, -.25, 0]} rotation={[-Math.PI/2,0,0]}>
+            <planeGeometry args={[2, 2]} />
+            <meshStandardMaterial/>
+        </mesh>
+        <mesh name='ceiling' position={[0, .25, 0]} rotation={[Math.PI/2,0,0]}>
+            <planeGeometry args={[2, 2]} />
+            <meshStandardMaterial/>
+        </mesh>
+        <mesh name='wallZ0' position={[0, 0, -1]} rotation={[0,0,0]}>
+            <planeGeometry args={[2, .5]} />
+            <meshStandardMaterial/>
+        </mesh>
+        <mesh name='wallZ1' position={[0, 0, 1]} rotation={[0,Math.PI,0]}>
+            <planeGeometry args={[2, .5]} />
+            <meshStandardMaterial/>
+        </mesh>
+        <mesh name='wallX0' position={[-1, 0, 0]} rotation={[0,Math.PI/2,0]}>
+            <planeGeometry args={[2, .5]} />
+            <meshStandardMaterial/>
+        </mesh>
+        <mesh name='wallX1' position={[1, 0, 0]} rotation={[0,-Math.PI/2,0]}>
+            <planeGeometry args={[2, .5]} />
+            <meshStandardMaterial/>
+        </mesh>
+    </object3D>
+}
+
 export default function Three() {
     return (
         <div>
@@ -35,11 +65,16 @@ export default function Three() {
                 }
             `}
             </style>
-            <Canvas>
-                <ambientLight />
-                <pointLight position={[10, 10, 10]} />
-                <Box position={[-1.2, 0, 0]} />
-                <Box position={[1.2, 0, 0]} />
+            <Canvas style={{display: 'block', width: '100%', height: '100%'}}
+            camera={{ position: [0,0,0], up: [0,1,0] }}>
+                <pointLight position={[0, .125, 0]}/>
+                <Room/>
+                <FirstPersonControls
+                    movementSpeed={0} lookSpeed={.25}
+                    constrainVertical={true}
+                    verticalMin={Math.PI/6}
+                    verticalMax={Math.PI*5/6}
+                    />
             </Canvas>
         </div>
     )
