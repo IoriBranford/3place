98389c32c250d408d250882826591f0ee6f8408c
IoriBranford <ioribranford@gmail.com>
2023-03-21 13:59:07 -0700

    Incorporate main page into 3D view as a splash screen.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,41 @@
-import Head from 'next/head';
+import React, { useState } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { FirstPersonControls } from '@react-three/drei'
 import styles from '../styles/Home.module.css';
+import Head from 'next/head';
 
-export default function Home() {
+function Room() {
+  return <object3D>
+    <mesh name='floor' position={[0, -.25, 0]} rotation={[-Math.PI / 2, 0, 0]}>
+      <planeGeometry args={[2, 2]} />
+      <meshStandardMaterial />
+    </mesh>
+    <mesh name='ceiling' position={[0, .25, 0]} rotation={[Math.PI / 2, 0, 0]}>
+      <planeGeometry args={[2, 2]} />
+      <meshStandardMaterial />
+    </mesh>
+    <mesh name='wallZ0' position={[0, 0, -1]} rotation={[0, 0, 0]}>
+      <planeGeometry args={[2, .5]} />
+      <meshStandardMaterial />
+    </mesh>
+    <mesh name='wallZ1' position={[0, 0, 1]} rotation={[0, Math.PI, 0]}>
+      <planeGeometry args={[2, .5]} />
+      <meshStandardMaterial />
+    </mesh>
+    <mesh name='wallX0' position={[-1, 0, 0]} rotation={[0, Math.PI / 2, 0]}>
+      <planeGeometry args={[2, .5]} />
+      <meshStandardMaterial />
+    </mesh>
+    <mesh name='wallX1' position={[1, 0, 0]} rotation={[0, -Math.PI / 2, 0]}>
+      <planeGeometry args={[2, .5]} />
+      <meshStandardMaterial />
+    </mesh>
+  </object3D>
+}
+
+function SplashScreen(props: { onClick: React.MouseEventHandler<HTMLDivElement>; }) {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>3place</title>
-        {/* <link rel="icon" href="/favicon.ico" /> */}
-      </Head>
+    <div className={styles.container} style={{ position: 'absolute', left: 0, top: 0, width: '100%' }}>
 
       <main>
         <h1 className={styles.title}>
@@ -19,33 +47,9 @@ export default function Home() {
         </p>
 
         <div className={styles.grid}>
-          <a href="/three" className={styles.card}>
-            {/* <h3>Documentation &rarr;</h3> */}
+          <div className={styles.card} onClick={props.onClick}>
             <p>ENTER</p>
-          </a>
-{/* 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a> */}
+          </div>
         </div>
       </main>
 
@@ -60,55 +64,91 @@ export default function Home() {
       </footer>
 
       <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-      `}</style>
+            main {
+              padding: 5rem 0;
+              flex: 1;
+              display: flex;
+              flex-direction: column;
+              justify-content: center;
+              align-items: center;
+            }
+            footer {
+              width: 100%;
+              height: 100px;
+              border-top: 1px solid #eaeaea;
+              display: flex;
+              justify-content: center;
+              align-items: center;
+            }
+            footer img {
+              margin-left: 0.5rem;
+            }
+            footer a {
+              display: flex;
+              justify-content: center;
+              align-items: center;
+              text-decoration: none;
+              color: inherit;
+            }
+            code {
+              background: #fafafa;
+              border-radius: 5px;
+              padding: 0.75rem;
+              font-size: 1.1rem;
+              font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+                DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+            }
+          `}</style>
 
       <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
+            html,
+            body {
+              padding: 0;
+              margin: 0;
+              font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+                Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+                sans-serif;
+            }
+            * {
+              box-sizing: border-box;
+            }
+          `}</style>
+    </div>
+  )
+}
+
+export default function ThreePlace() {
+  const [started, setStarted] = useState(false)
+  return (
+    <div>
+      <Head>
+        <title>3place</title>
+      </Head>
+      <style jsx global>{`
+                html,
+                body,
+                body > div:first-child,
+                div#__next,
+                div#__next > div {
+                    padding: 0;
+                    margin: 0;
+                    height: 100%;
+                }
+            `}
+      </style>
+      <Canvas style={{ display: 'block', width: '100%', height: '100%' }}
+        camera={{ position: [0, 0, 0], up: [0, 1, 0] }}>
+        <pointLight position={[0, .125, 0]} />
+        <Room />
+        <FirstPersonControls
+          enabled={started}
+          movementSpeed={0} lookSpeed={.25}
+          constrainVertical={true}
+          verticalMin={Math.PI / 6}
+          verticalMax={Math.PI * 5 / 6}
+        />
+      </Canvas>
+      {started ? null : (<SplashScreen onClick={() => setStarted(true)} />)}
     </div>
   )
 }
