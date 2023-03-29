df385d7c510065ec8f833d4eedc44cf379ce2f3b
IoriBranford <ioribranford@gmail.com>
2023-03-28 20:47:54 -0700

    Un-nest Surface and Room components.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,76 @@
-import React, { MutableRefObject, useRef, useState, useLayoutEffect } from 'react'
+import React, { MutableRefObject, useRef, useLayoutEffect, createContext, useContext } from 'react'
 import { Canvas, ThreeEvent, useFrame } from '@react-three/fiber'
-import { FirstPersonControls, ScreenSpace, useTexture } from '@react-three/drei'
+import { FirstPersonControls, Plane, useTexture } from '@react-three/drei'
 import Head from 'next/head';
-import { BufferAttribute, Group, Mesh, MeshStandardMaterial, PlaneGeometry, RepeatWrapping, Texture, Vector3 } from 'three';
+import { BufferAttribute, Mesh, MeshStandardMaterial, RepeatWrapping, Texture } from 'three';
 import { SplashScreen } from '../components/SplashScreen';
 
-export function Scene(props) {
+const SceneContext = createContext(null)
+
+function Surface({
+  name = '',
+  position,
+  rotation,
+  width = 1,
+  height = 1,
+  image = '',
+}) {
+  const mesh = useRef<Mesh>(null!)
+  const context = useContext(SceneContext)
+  useLayoutEffect(()=> {
+    const uv = mesh.current.geometry.getAttribute('uv') as BufferAttribute
+    uv.setXY(0, 0, height)
+    uv.setXY(1, width, height)
+    uv.setXY(2, 0, 0)
+    uv.setXY(3, width, 0)
+  })
+  const texture = image == '' ? null : useTexture(image, (texture:Texture) => {
+    texture.wrapS = texture.wrapT = RepeatWrapping
+  })
+  return (
+    <Plane name={name} ref={mesh} position={position} rotation={rotation}
+      onClick={()=>{context.onClickSurface(mesh)}} args={[width, height]}>
+      <meshStandardMaterial map={texture} />
+    </Plane>
+  )
+}
+
+function Room() {
+  let images = {
+    floor: 'Bricks_17-512x512.png',
+    ceiling: 'Bricks_17-512x512.png',
+    wallX0: 'Wood_02-512x512.png',
+    wallX1: 'Wood_02-512x512.png',
+    wallZ0: 'Wood_02-512x512.png',
+    wallZ1: 'Wood_02-512x512.png',
+  }
+  return <>
+    <Surface name='floor' position={[0, -.5, 0]} rotation={[-Math.PI / 2, 0, 0]}
+      width={4} height={4} image={images.floor}/>
+    <Surface name='ceiling' position={[0, .5, 0]} rotation={[Math.PI / 2, 0, 0]}
+      width={4} height={4} image={images.ceiling} />
+    <Surface name='wallZ0' position={[0, 0, -2]} rotation={[0, 0, 0]}
+      width={4} height={1} image={images.wallZ0} />
+    <Surface name='wallZ1' position={[0, 0, 2]} rotation={[0, Math.PI, 0]}
+      width={4} height={1} image={images.wallZ1} />
+    <Surface name='wallX0' position={[-2, 0, 0]} rotation={[0, Math.PI / 2, 0]}
+      width={4} height={1} image={images.wallX0} />
+    <Surface name='wallX1' position={[2, 0, 0]} rotation={[0, -Math.PI / 2, 0]}
+      width={4} height={1} image={images.wallX1} />
+    <pointLight position={[0, .25, 0]} />
+  </>
+}
+
+export function Scene({controlsRef}) {
   let pointedObject:MutableRefObject<Mesh> = null!
   let clickedObject:MutableRefObject<Mesh> = null!
-  const textureSelector = useRef<Group>(null!)
+  // const textureSelector = useRef<Group>(null!)
 
   function onClickTexture(event:ThreeEvent<MouseEvent>) {
     event.stopPropagation()
     clickedObject = null
-    textureSelector.current.visible = false
-    props.controlsRef.current.enabled = true
+    // textureSelector.current.visible = false
+    controlsRef.current.enabled = true
   }
 
   const onClickSurface = (mesh: MutableRefObject<Mesh>) => {
@@ -23,122 +79,52 @@ export function Scene(props) {
       standardMaterial.color.set('white')
     }
     clickedObject = (clickedObject == mesh ? null : mesh)
-    textureSelector.current.visible = (clickedObject != null)
-    props.controlsRef.current.enabled = (clickedObject == null)
+    // textureSelector.current.visible = (clickedObject != null)
+    controlsRef.current.enabled = (clickedObject == null)
   }
 
-  const onPointerOver = (mesh: MutableRefObject<Mesh>) => {
+  const onPointerOverSurface = (mesh: MutableRefObject<Mesh>) => {
     if (pointedObject !== mesh) {
       pointedObject = mesh
     }
   }
 
-  const onPointerOut = (mesh: MutableRefObject<Mesh>) => {
+  const onPointerOutSurface = (mesh: MutableRefObject<Mesh>) => {
     if (pointedObject === mesh) {
       pointedObject = null
     }
   }
 
-  const [surfaceImages, setSurfaceImages] = useState({
-    floor: 'Bricks_17-512x512.png',
-    ceiling: 'Bricks_17-512x512.png',
-    wallX0: 'Wood_02-512x512.png',
-    wallX1: 'Wood_02-512x512.png',
-    wallZ0: 'Wood_02-512x512.png',
-    wallZ1: 'Wood_02-512x512.png',
+  useFrame((state, delta) => {
+    if (clickedObject) {
+      const standardMaterial = clickedObject.current.material as MeshStandardMaterial
+      const pulse = (3 + Math.cos(state.clock.elapsedTime * 8*Math.PI)) / 4
+      standardMaterial.color.setScalar(pulse)
+    }
   })
-  function changeSurfaceImages(changes) {
-    for (const key in surfaceImages) {
-      if (!changes[key])
-        changes[key] = surfaceImages[key]
-    }
-    setSurfaceImages(changes)
-  }
-  const surfaceTextures = {}
-  for (const key in surfaceImages) {
-    surfaceTextures[key] = useTexture(surfaceImages[key],
-      (texture:Texture) => {
-        texture.wrapS = texture.wrapT = RepeatWrapping
-      }
-    )
-  }
-  // console.log(surfaceTextures)
-  
-  function Surface(props) {
-    const mesh = useRef<Mesh>(null!)
-    const standardMaterial = useRef<MeshStandardMaterial>(null!)
-    const planeGeometry = useRef<PlaneGeometry>(null!)
-    let width = 1
-    let height = 1
-    if (props.planeGeometry) {
-      width  = props.planeGeometry[0]
-      height = props.planeGeometry[1]
-    }
-    useLayoutEffect(()=> {
-      const uv = planeGeometry.current.getAttribute('uv') as BufferAttribute
-      uv.setXY(0, 0, height)
-      uv.setXY(1, width, height)
-      uv.setXY(2, 0, 0)
-      uv.setXY(3, width, 0)
-    })
-    return (
-      <mesh ref={mesh} position={props.position} rotation={props.rotation}
-        onClick={()=> onClickSurface(mesh)}
-        onPointerOver={() => onPointerOver(mesh)}
-        onPointerOut={() => onPointerOut(mesh)}
-        >
-        <planeGeometry ref={planeGeometry} args={[width, height]} />
-        <meshStandardMaterial ref={standardMaterial} map={surfaceTextures[props.name]} />
-      </mesh>
-    )
-  }
 
-  function Room() {
-    useFrame((state, delta) => {
-      if (clickedObject) {
-        const standardMaterial = clickedObject.current.material as MeshStandardMaterial
-        const pulse = (3 + Math.cos(state.clock.elapsedTime * 8*Math.PI)) / 4
-        standardMaterial.color.setScalar(pulse)
-      }
-    })
-
-    return <object3D>
-      <Surface name='floor' position={[0, -.5, 0]} rotation={[-Math.PI / 2, 0, 0]}
-        planeGeometry={[4, 4]} />
-      <Surface name='ceiling' position={[0, .5, 0]} rotation={[Math.PI / 2, 0, 0]}
-        planeGeometry={[4, 4]} />
-      <Surface name='wallZ0' position={[0, 0, -2]} rotation={[0, 0, 0]}
-        planeGeometry={[4, 1]} />
-      <Surface name='wallZ1' position={[0, 0, 2]} rotation={[0, Math.PI, 0]}
-        planeGeometry={[4, 1]} />
-      <Surface name='wallX0' position={[-2, 0, 0]} rotation={[0, Math.PI / 2, 0]}
-        planeGeometry={[4, 1]} />
-      <Surface name='wallX1' position={[2, 0, 0]} rotation={[0, -Math.PI / 2, 0]}
-        planeGeometry={[4, 1]} />
-      <pointLight position={[0, .25, 0]} />
-    </object3D>
-  }
-
-  const depth = 1/8
-  const scale = new Vector3(1/32, 1/32, 1)
-  function TextureSelector() {
-    return (
-      <ScreenSpace ref={textureSelector} depth={depth} scale={scale} visible={false}>
-        <sprite position={[6, 0, 0]} onClick={onClickTexture}>
-          <spriteMaterial map={surfaceTextures['floor']}/>
-        </sprite>
-        <sprite position={[6, 1, 0]} onClick={onClickTexture}>
-          <spriteMaterial map={surfaceTextures['wallX0']}/>
-        </sprite>
-      </ScreenSpace>
-    )
-  }
+  // const depth = 1/8
+  // const scale = new Vector3(1/32, 1/32, 1)
+  // function TextureSelector() {
+  //   return (
+  //     <ScreenSpace ref={textureSelector} depth={depth} scale={scale} visible={false}>
+  //       <sprite position={[6, 0, 0]} onClick={onClickTexture}>
+  //         <spriteMaterial map={surfaceTextures['floor']}/>
+  //       </sprite>
+  //       <sprite position={[6, 1, 0]} onClick={onClickTexture}>
+  //         <spriteMaterial map={surfaceTextures['wallX0']}/>
+  //       </sprite>
+  //     </ScreenSpace>
+  //   )
+  // }
 
   return (
-    <>
+    <SceneContext.Provider value={{
+      onClickSurface:onClickSurface
+    }}>
       <Room />
-      <TextureSelector/>
-    </>
+      {/* <TextureSelector/> */}
+    </SceneContext.Provider>
   )
 }
 
