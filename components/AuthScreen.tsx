70329f62904607a2526df22633129c4a970b0e9b
IoriBranford <ioribranford@gmail.com>
2023-05-30 14:32:15 -0700

    Hide AuthScreen when not the active menu.

--- components/AuthScreen.tsx
+++ components/AuthScreen.tsx
@@ -2,10 +2,15 @@ import { Auth } from '@supabase/auth-ui-react'
 import { ThemeSupa } from '@supabase/auth-ui-shared'
 import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react'
 import Account from './Account'
+import { GuiContext } from './Gui'
+import { useContext } from 'react'
 
 const AuthScreen = () => {
   const session = useSession()
   const supabase = useSupabaseClient()
+  const gui = useContext(GuiContext)
+  if (gui.activeMenu != 'AuthScreen')
+    return <></>
 
   return (
     <div className="container" style={{ position: 'absolute', left: 0, top: 0, width: '100%', padding: '50px 0 100px 0' }}>
