f18af9948e727105ad1f27e29fed99d721930d7e
IoriBranford <ioribranford@gmail.com>
2023-06-02 12:31:18 -0700

    Remove old code.

--- contexts/Editor.tsx
+++ contexts/Editor.tsx
@@ -2,59 +2,23 @@ import { createContext } from "react"
 import { Mesh, MeshStandardMaterial, Object3D, Texture } from "three"
 import { GuiState } from "../components/Gui"
 
-export class Editor {
-    private selectedMesh: Mesh = null!
-    private pointedMesh: Mesh = null!
-
+class Editor {
     guiState: GuiState = null!
 
-    onPointerOnMesh(mesh: Mesh) {
-        if (this.pointedMesh !== mesh) {
-            this.pointedMesh = mesh
-        }
-    }
-
-    onPointerOffMesh(mesh: Mesh) {
-        if (this.pointedMesh === mesh) {
-            this.pointedMesh = null!
-        }
-    }
-
-    onClickMesh(mesh: Mesh): void {
-        this.setSelectedMesh(mesh)
-        if (this.guiState)
-            this.guiState.setActiveMenu('TextureMenu')
-    }
-
-    flashSelectedMesh(elapsedTime: number): void {
-        if (this.selectedMesh) {
-            const standardMaterial = this.selectedMesh.material as MeshStandardMaterial
-            const pulse = (3 + Math.cos(elapsedTime * 8 * Math.PI)) / 4
-            standardMaterial.color.setScalar(pulse)
-        }
-    }
-
-    isMeshSelected(): boolean {
-        return this.selectedMesh != null
-    }
-
-    setSelectedMeshTexture(texture: Texture) {
-        if (this.selectedMesh) {
-            const standardMaterial = this.selectedMesh.material as MeshStandardMaterial
-            standardMaterial.map = texture
-            standardMaterial.needsUpdate = true
-        }
-    }
-
-    setSelectedMesh(mesh: Mesh) {
-        if (this.selectedMesh) {
-            const standardMaterial = this.selectedMesh.material as MeshStandardMaterial
-            standardMaterial.color.set('white')
-        }
-        this.selectedMesh = mesh
-    }
-
     private selectedObject: Object3D = null!
+    private pointedObject: Object3D = null!
+
+    onPointerOnObject(object: Object3D) {
+        if (this.pointedObject !== object) {
+            this.pointedObject = object
+        }
+    }
+
+    onPointerOffObject(object: Object3D) {
+        if (this.pointedObject === object) {
+            this.pointedObject = null!
+        }
+    }
 
     onClickObject(object: Object3D): void {
         this.setSelectedObject(object)
--- pages/room/[roomId].tsx
+++ pages/room/[roomId].tsx
@@ -3,68 +3,10 @@ import React, { useRef, useLayoutEffect, useContext } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
 import { FirstPersonControls, Plane } from '@react-three/drei'
 import Head from 'next/head';
-import { BufferAttribute, Mesh } from 'three';
-import { AssetsContext } from '../../contexts/Assets';
 import { EditorContext } from '../../contexts/Editor';
 import { FirstPersonControls as FirstPersonControlImpl } from 'three-stdlib';
 import { Gui } from '../../components/Gui';
 import RectangleRoom from "../../components3D/RectangleRoom";
-
-function Surface({
-    name = '',
-    position,
-    rotation,
-    width = 1,
-    height = 1,
-    image = '',
-}) {
-    const mesh = useRef<Mesh>(null!)
-    const editor = useContext(EditorContext)
-    const assets = useContext(AssetsContext)
-    useLayoutEffect(() => {
-        const uv = mesh.current.geometry.getAttribute('uv') as BufferAttribute
-        uv.setXY(0, 0, height)
-        uv.setXY(1, width, height)
-        uv.setXY(2, 0, 0)
-        uv.setXY(3, width, 0)
-    })
-    const texture = image == '' ? null : assets.getTexture(image)
-    function onClick() {
-        editor.onClickMesh(mesh.current)
-    }
-    return (
-        <Plane name={name} ref={mesh} position={position} rotation={rotation}
-            onClick={onClick} args={[width, height]}>
-            <meshStandardMaterial map={texture} />
-        </Plane>
-    )
-}
-
-function DefaultRoom() {
-    let images = {
-        floor: '/bricks.png',
-        ceiling: '/bricks.png',
-        wallX0: '/wood.png',
-        wallX1: '/wood.png',
-        wallZ0: '/wood.png',
-        wallZ1: '/wood.png',
-    }
-    return <object3D>
-        <Surface name='floor' position={[0, -.5, 0]} rotation={[-Math.PI / 2, 0, 0]}
-            width={4} height={4} image={images.floor} />
-        <Surface name='ceiling' position={[0, .5, 0]} rotation={[Math.PI / 2, 0, 0]}
-            width={4} height={4} image={images.ceiling} />
-        <Surface name='wallZ0' position={[0, 0, -2]} rotation={[0, 0, 0]}
-            width={4} height={1} image={images.wallZ0} />
-        <Surface name='wallZ1' position={[0, 0, 2]} rotation={[0, Math.PI, 0]}
-            width={4} height={1} image={images.wallZ1} />
-        <Surface name='wallX0' position={[-2, 0, 0]} rotation={[0, Math.PI / 2, 0]}
-            width={4} height={1} image={images.wallX0} />
-        <Surface name='wallX1' position={[2, 0, 0]} rotation={[0, -Math.PI / 2, 0]}
-            width={4} height={1} image={images.wallX1} />
-        <pointLight position={[0, .25, 0]} />
-    </object3D>
-}
 
 export function Scene({ roomData = null }) {
     const editor = useContext(EditorContext)
