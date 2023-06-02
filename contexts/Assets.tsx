2eaadd5deda8105bea6ff18485640248e07c5bb8
IoriBranford <ioribranford@gmail.com>
2023-06-02 13:53:52 -0700

    Let useLoader cache the textures.

--- contexts/Assets.tsx
+++ contexts/Assets.tsx
@@ -1,17 +1,13 @@
+import { useLoader } from "@react-three/fiber";
 import { createContext } from "react";
 import { TextureLoader, Texture, RepeatWrapping, sRGBEncoding } from "three";
 
 export class Assets {
-    private textures = {}
-    private textureLoader = new TextureLoader()
-
     getTexture(url: string): Texture {
-        let texture = this.textures[url] as Texture
-        if (!texture) {
-            texture = this.textureLoader.load(url)
+        let texture = useLoader(TextureLoader, url)
+        if (texture) {
             texture.wrapS = texture.wrapT = RepeatWrapping
             texture.encoding = sRGBEncoding
-            this.textures[url] = texture
         }
         return texture
     }
